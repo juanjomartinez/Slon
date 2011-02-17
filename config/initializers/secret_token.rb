351e04d7ecd4aeb316050c7361548c42a07e79b5
Juanjo <juanjo.martinez@sci-spain.com>
2011-02-17 09:08:40 +0000

    creación del proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slon::Application.config.secret_token = 'bbe509f537f4dc094e50e39e373fc39a6a73a12ae137ed2bb414415fd2575090997bd63dcb542a96999f483e7aa5191e39980ddbe6e584534856d80cdfccb795'
